4609347b3eb73d54998c60d670298e2bf7989978
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2024-09-24 12:45:49 +0530

    Backend setup with Firebase

---
+++ configs/FirebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBiLJZZx0rmcbWc9bGeJHqf-SLW1K3e48E",
+  authDomain: "projects-bb966.firebaseapp.com",
+  projectId: "projects-bb966",
+  storageBucket: "projects-bb966.appspot.com",
+  messagingSenderId: "230850881756",
+  appId: "1:230850881756:web:02e2e25b97ccac394b6c19",
+  measurementId: "G-Z1NY1HT2Q6"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
