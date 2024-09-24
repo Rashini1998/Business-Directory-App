884a80a1908c41c77b5f6467423a54bce9d50e65
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2024-09-24 13:24:39 +0530

    Add Home Screen Slider

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
+import Slider from '../../components/Home/Slider'
 
 export default function home() {
   return (
@@ -9,6 +10,7 @@ export default function home() {
       <Header/>
 
       {/* Slider */}
+      <Slider/>
 
       {/* Category */}
 
