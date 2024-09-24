cbe8124d0f3d4dd425293a8175e66ac3e3d7d6fd
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2024-09-24 09:13:59 +0530

    Tab Navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,14 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)"/>
+    <Stack screenOptions={{
+      headerShown:false
+    }}>
+      <Stack.Screen name="(tabs)" 
+      // options={{
+      //   headerShown:false
+      // }}
+      />
     </Stack>
   );
 }
