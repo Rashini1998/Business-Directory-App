c978cc79220cf0a0932f9f48462dd7fc99c66f02
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2024-09-27 10:38:01 +0530

    Complete Display Business List By Category

---
+++ app/businesslist/[category].js
@@ -0,0 +1,50 @@
+// Dynamic route -> businesslist/categoryName
+
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useLocalSearchParams, useNavigation } from 'expo-router'
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../../configs/FirebaseConfig';
+import BusinessCard from '../../components/BusinessList/BusinessCard';
+import { Colors } from '../../constants/Colors';
+
+
+export default function BusinessListByCategory() {
+    const navigation=useNavigation();
+    const {category}=useLocalSearchParams();
+    const [businessList,setBusinessList]=useState([]);
+    useEffect(()=>{
+        navigation.setOptions({
+            headerShown:true,
+            headerTitle:category
+        });
+        GetBusinessList();
+    },[]);
+
+    const GetBusinessList=async()=>{
+        setBusinessList([]);
+        const q=query(collection(db,'BusinessList'),where("category",'==',category));
+        const querySnapshot=await getDocs(q);
+
+        querySnapshot.forEach((doc)=>{
+            setBusinessList(prev=>[...prev,doc.data()]);
+        })
+    }
+  return (
+    <View>
+     {businessList?.length>0? <FlatList
+      data={businessList}
+      renderItem={({item,index})=>(
+        <BusinessCard business={item} key={index}/>
+      )}
+      />:
+      <Text style={{
+        fontSize:20,
+        fontFamily:'outfit-bold',
+        color:Colors.GRAY,
+        textAlign:'center',
+        marginTop:'50%'
+      }}>No Business Found</Text>}
+    </View>
+  )
+}
--- components/Home/Category.jsx
+++ components/Home/Category.jsx
@@ -5,9 +5,11 @@ import {Colors} from '../../constants/Colors.ts'
 import CategoryItem from './CategoryItem.jsx';
 import { collection,getDocs,query } from 'firebase/firestore';
 import {db} from '../../configs/FirebaseConfig.js';
+import { useRouter } from 'expo-router';
 
 export default function Category() {
     const [categoryList, setCategoryList] = useState([]);
+    const router=useRouter();
     useEffect(()=>{
         GetCategoryList();
     },[]);
@@ -42,7 +44,7 @@ export default function Category() {
         style={{marginLeft:20}}
         horizontal={true}
         renderItem={({item,index})=>(
-            <CategoryItem category={item} key={index} onCategoryPress={(category)=>console.log(category)}/>
+            <CategoryItem category={item} key={index} onCategoryPress={(category)=>router.push('/businesslist/'+item.name)}/>
         )}
       />
 
